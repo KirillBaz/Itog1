ed9b8cdd241f13a6af15ffb759862dcd700fc83f
Kirill <kirill.bazyura@yandex.ru>
2022-11-17 14:31:44 +0300

    создан метод, принимающий и отображающий исходный массив

--- ИтоговаяРабота/Program.cs
+++ ИтоговаяРабота/Program.cs
@@ -1,1 +1,17 @@
 ﻿// Написать программу, которая из имеющегося строкового массива сформирует массив из строк, длина которых не превышает 3 элемента
+string[] EnterArray(int size1)
+{
+    var array1 = new string[size1];
+    for (int i = 0; i < size1; i++)
+    {
+        Console.WriteLine($"Введите {i+1}ю строку");
+        array1[i]=Console.ReadLine();
+    }
+Console.Write("Введен массив: [");
+for (int j = 0; j < size1-1; j++)
+{
+    Console.Write($"{array1[j]}, ");
+}
+Console.WriteLine($"{array1[size1-1]}]");
+return array1;
+}
